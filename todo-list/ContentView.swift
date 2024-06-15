11880ef956274317ec4d45227dd2d4d72a0ccd54
yongsanpawat <koyong7773@gmail.com>
2024-06-15 17:48:20 +0700

    develop create todo

--- todo-list/ContentView.swift
+++ todo-list/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
         Todo(title: "Meeting with Tom", isDone: false),
         Todo(title: "Math examination", isDone: false)
     ]
+    @State private var showCreateForm: Bool = false
+    @State private var todoTitle: String = ""
     
     var body: some View {
         VStack {
@@ -28,7 +30,7 @@ struct ContentView: View {
                     Spacer()
                     
                     Button(action: {
-                        
+                        todo.isDone.toggle()
                     }, label: {
                         Image(systemName: todo.isDone ? "checkmark.circle.fill" : "checkmark.circle")
                             .resizable()
@@ -38,8 +40,27 @@ struct ContentView: View {
                 .padding()
                 .background(Color.black.opacity(0.08))
             }
+            
+            Spacer()
+            
+            Button("New Todo") {
+                showCreateForm = true
+            }
         }
         .padding()
+        .sheet(isPresented: $showCreateForm, content: {
+            VStack {
+                TextField("Title", text: $todoTitle)
+                    .textFieldStyle(.roundedBorder)
+                
+                Button("Create") {
+                    todoList.append(Todo(title: todoTitle, isDone: false))
+                    showCreateForm = false
+                    todoTitle = ""
+                }
+            }
+            .padding()
+        })
     }
 }
 
