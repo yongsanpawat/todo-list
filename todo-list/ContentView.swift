00f6bc39d918c9032c6732b45b914c41c07f3e40
yongsanpawat <koyong7773@gmail.com>
2024-06-15 16:52:14 +0700

    develop todo list

--- todo-list/ContentView.swift
+++ todo-list/ContentView.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var todoList: [Todo] = [
+        Todo(title: "Meeting with Tom", isDone: false),
+        Todo(title: "Math examination", isDone: false)
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("My Todo List")
+                .font(.title)
+            
+            ForEach(todoList) { todo in
+                HStack {
+                    Text(todo.title)
+                        .font(.title3)
+                        .foregroundStyle(.blue)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: todo.isDone ? "checkmark.circle.fill" : "checkmark.circle")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                    })
+                }
+                .padding()
+                .background(Color.black.opacity(0.08))
+            }
         }
         .padding()
     }
